0fd353e2508762841770752ec26d3194110a31e4
jas410 <adamsjasmine132@example.com>
2026-08-03 02:52:29 -0400

    Remove broken Mini Labs folder

--- Mini Labs/S19-Pipeline-Lab/main.tf
+++
@@ -1,7 +0,0 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_s3_bucket" "lab_bucket" {
-  bucket = "jasmine-s19-lab-bucket"
-}
--- Mini Labs/S21-Delivery-Lab/main.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {  
-  region = "us-east-1"  
-}  
-  
-resource "aws_ssm_parameter" "drone_check" {  
-  name  = "/tkh/pipeline/status"  
-  type  = "String"  
-  value = "Deployment Drone Successful via OIDC"  
-}  
