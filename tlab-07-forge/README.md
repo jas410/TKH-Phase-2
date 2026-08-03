867e735bcad42193f59b36f47e2ed1942dbcbf43
jas410 <adamsjasmine132@example.com>
2026-08-03 02:30:51 -0400

    Add TLAB7 Forge project (fixed folder)

---
+++ tlab-07-forge/README.md
@@ -0,0 +1,4 @@
+# trigger
+# trigger
+# trigger
+# final oidc fix
---
+++ tlab-07-forge/main.tf
@@ -0,0 +1,15 @@
+provider "aws" {  
+  region = "us-east-1"  
+}  
+  
+resource "aws_security_group" "sabotaged_sg" {  
+  name        = "tlab7-exposed-sg"  
+  description = "A dangerously exposed security group"  
+  
+  ingress {  
+    from_port   = 22  
+    to_port     = 22  
+    protocol    = "tcp"  
+    cidr_blocks = ["172.56.36.64/27"] # SABOTAGE: SSH exposed to the world  
+  }  
+}  
